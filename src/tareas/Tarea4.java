ce9aff7f295780998bd5485f102eb05e093d58cb
jsanmarco06 <jsanmarco@americavirtualsa.com>
2023-02-14 14:56:13 -0300

    Subiendo tarea 4

--- src/tareas/Tarea4.java
+++ src/tareas/Tarea4.java
@@ -3,48 +3,28 @@ package tareas;
 import java.util.Scanner;
 
 public class Tarea4 {
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		String nombreUno, nombreDos, nombreTres;
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Ingrese el primer nombre:");
+		System.out.println("Ingrese Un nombre:");
 		nombreUno = scanner.nextLine();
-		
-		System.out.println("Ingrese el segundo nombre:");
+		System.out.println("Ingrese Un Segundo nombre:");
 		nombreDos = scanner.nextLine();
-		
-		System.out.println("Ingrese el tercer nombre:");
+		System.out.println("Ingrese Un Tercer nombre:");
 		nombreTres = scanner.nextLine();
 		
-		if((nombreUno.length() >= nombreDos.length()) && (nombreUno.length() > nombreTres.length())) {
-			System.out.println("Nombres Ordenados por longitud:\n" + nombreUno);
-			if((nombreDos.length() >= nombreTres.length())) {
-				System.out.println(nombreDos + "\n"+ nombreTres + "\n");
-			}
-			if((nombreDos.length() < nombreTres.length())) {
-				System.out.println(nombreTres + "\n"+ nombreDos + "\n");
-			}
+		if((nombreUno.length() >= nombreDos.length()) && (nombreUno.length() >= nombreTres.length())) {
+			System.out.println("El nombre mas largo es " + nombreUno + " con " + nombreUno.length() + " caracteres");
 		}
-		if((nombreDos.length() >= nombreUno.length()) && (nombreDos.length() > nombreTres.length())) {
-			System.out.println("Nombres Ordenados por longitud:\n" + nombreDos);
-			if((nombreUno.length() >= nombreTres.length())) {
-				System.out.println(nombreUno + "\n"+ nombreTres + "\n");
-			}
-			if((nombreUno.length() < nombreTres.length())) {
-				System.out.println(nombreTres + "\n"+ nombreUno + "\n");
-			}
+		else if((nombreDos.length() >= nombreUno.length()) && (nombreDos.length() >= nombreTres.length())) {
+			System.out.println("El nombre mas largo es " + nombreDos + " con  " + nombreDos.length() + " caracteres");
 		}
-		if((nombreTres.length() >= nombreUno.length()) && (nombreTres.length() > nombreDos.length())) {
-			System.out.println("Nombres Ordenados por longitud:\n" + nombreTres);
-			if((nombreUno.length() >= nombreDos.length())) {
-				System.out.println(nombreUno + "\n"+ nombreDos + "\n");
-			}
-			if((nombreUno.length() < nombreDos.length())) {
-				System.out.println(nombreDos + "\n"+ nombreUno + "\n");
-			}
+		else if((nombreTres.length() >= nombreUno.length()) && (nombreTres.length() >= nombreDos.length())) {
+			System.out.println("El nombre mas largo es " + nombreTres + " con " + nombreTres.length() + " caracteres");
 		}
-		if((nombreUno.length() == nombreDos.length()) && (nombreTres.length() == nombreDos.length())) {
-			System.out.println("Nombres Ordenados por longitud:\n" + nombreTres + "\n"+ nombreUno + "\n"+ nombreDos + "\n");
-		}
+
 	}
+
 }
